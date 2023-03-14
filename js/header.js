b0729ed54d45f74375cd664109ae3775fd5e51a8
foo2023 <124128496+foo2023@users.noreply.github.com>
2023-03-14 17:59:21 +0800

    Update header.js

--- js/header.js
+++ js/header.js
@@ -12,14 +12,14 @@ navHeader.innerHTML = `
 </label>
 <label class="logo">Practice</label>
 <ul>
-<li><a class="active nav-btn-font" href="home.html" id="home">HOME</a></li>
+<li><a class="active nav-btn-font" href="index.html" id="home">HOME</a></li>
 <li><a href="menu.html" class="hide nav-btn-font" id="menu">MENU</a></li>
 <li><a href="#" class="hide nav-btn-font" id="about">ABOUT</a></li>
 <li><a href="#" class="hide nav-btn-font" id="download">DOWNLOAD</a></li>
 </ul>
 <ul class="seperate">
 <div class="seperate-container">
-<li  id="mobileHome"><a href="home.html" class="nav-btn-font">HOME</a></li>
+<li  id="mobileHome"><a href="index.html" class="nav-btn-font">HOME</a></li>
 <li  id="mobileMenu"><a href="menu.html" class="nav-btn-font">MENU</a></li>
 <li  id="mobileAbout"><a href="#" class="nav-btn-font">ABOUT</a></li>
 <li  id="mobileDownload"><a href="#" class="nav-btn-font">DOWNLOAD</a></li>
