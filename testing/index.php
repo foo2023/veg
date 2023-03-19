f1f52947372c45e9518c8b919d270abb1d80e16f
foo2023 <124128496+foo2023@users.noreply.github.com>
2023-03-19 17:05:23 +0800

    Create index.php

---
+++ testing/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    
+    <head>
+        <title>
+        </title>
+    </head>
+    <body>
+        <form action="upload.php" method="POST" enctype="multipart/form-data">
+            <input type="file" name="file">
+            <button type="submit" name="submit">UPLOAD</button>
+        </form>
+        <div id="uploaded-images">
+            <!-- Display uploaded images here -->
+            </div>
+    </body>
+</html>
+
