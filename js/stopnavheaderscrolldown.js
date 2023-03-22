2bb12b75af21a886ae8bb29b8a27429c5b21a2cc
foo2023 <124128496+foo2023@users.noreply.github.com>
2023-03-23 07:16:06 +0800

    Add files via upload

---
+++ js/stopnavheaderscrolldown.js
@@ -0,0 +1,17 @@
+    function stopNavheaderScrollDown() {
+        const signalNav = document.getElementById("signalNav")
+        if (signalNav.classList.contains("signalNav")) {
+            signalNav.classList.remove("signalNav");
+        } else {
+            signalNav.classList.add("signalNav");
+        }
+    }
+    function changePosition() {
+        const checkbtn = document.getElementById("checkbtn")
+        if (checkbtn.style.position === "static") {
+            checkbtn.style.position = "static";
+        } else {
+            checkbtn.style.position = "static";
+        }
+        
+    }
