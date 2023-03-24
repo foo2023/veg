59c3668c8753266ebbd4f66e663d93f3d957b62d
foo2023 <124128496+foo2023@users.noreply.github.com>
2023-03-24 14:45:28 +0800

    Update stopnavheaderscrolldown.js

--- js/stopnavheaderscrolldown.js
+++ js/stopnavheaderscrolldown.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
     function stopNavheaderScrollDown() {
         const signalNav = document.getElementById("signalNav")
         if (signalNav.classList.contains("signalNav")) {
@@ -14,4 +15,22 @@
             checkbtn.style.position = "static";
         }
         
+=======
+    function stopNavheaderScrollDown() {
+        const signalNav = document.getElementById("signalNav")
+        if (signalNav.classList.contains("signalNav")) {
+            signalNav.classList.remove("signalNav");
+        } else {
+            signalNav.classList.add("signalNav");
+        }
+    }
+    function changePosition() {
+        const checkbtn = document.getElementById("checkbtn")
+        if (checkbtn.style.position === "static") {
+            checkbtn.style.position = "static";
+        } else {
+            checkbtn.style.position = "static";
+        }
+        
+>>>>>>> 2bb12b75af21a886ae8bb29b8a27429c5b21a2cc
     }
