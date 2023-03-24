fb900a6edad22c6ee94f60f6a677f9e571262e4f
foo2023 <124128496+foo2023@users.noreply.github.com>
2023-03-24 14:45:30 +0800

    Update naviconanimation.js

--- js/naviconanimation.js
+++ js/naviconanimation.js
@@ -1,3 +1,8 @@
+<<<<<<< HEAD
     function myfc(x) {
         x.classList.toggle("iconchange");
+=======
+    function myfc(x) {
+        x.classList.toggle("iconchange");
+>>>>>>> 2bb12b75af21a886ae8bb29b8a27429c5b21a2cc
     }
