4c68d71ed8d12b40372d8c4d00976368ae6e47b4
foo2023 <124128496+foo2023@users.noreply.github.com>
2023-03-24 14:45:21 +0800

    Update itemHeader.js

--- js/itemHeader.js
+++ js/itemHeader.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 navHeader.innerHTML = `
 <nav class="navbar" id="signalNav">
 <input type="checkbox" id="check">
@@ -27,4 +28,35 @@ navHeader.innerHTML = `
 </ul>
 </nav> `
 
+=======
+navHeader.innerHTML = `
+<nav class="navbar" id="signalNav">
+<input type="checkbox" id="check">
+<label for="check" class="checkbtn" id="checkbtn">
+<a onclick="myfc(this); changePosition(); stopNavheaderScrollDown();">
+<div  class="navm">
+<div class="bar1"></div>
+<div class="bar2"></div>
+<div class="bar3"></div>
+</div>
+          </a>
+</label>
+<label class="logo">Practice</label>
+<ul>
+<li><a class="active nav-btn-font" href="../home.html" id="home">HOME</a></li>
+<li><a href="../menu.html" class="hide nav-btn-font" id="menu">MENU</a></li>
+<li><a href="#" class="hide nav-btn-font" id="about">ABOUT</a></li>
+<li><a href="#" class="hide nav-btn-font" id="download">DOWNLOAD</a></li>
+</ul>
+<ul class="seperate">
+<div class="seperate-container">
+<li  id="mobileHome"><a href="../home.html" class="nav-btn-font">HOME</a></li>
+<li  id="mobileMenu"><a href="../menu.html" class="nav-btn-font">MENU</a></li>
+<li  id="mobileAbout"><a href="#" class="nav-btn-font">ABOUT</a></li>
+<li  id="mobileDownload"><a href="#" class="nav-btn-font">DOWNLOAD</a></li>
+</div>
+</ul>
+</nav> `
+
+>>>>>>> 2bb12b75af21a886ae8bb29b8a27429c5b21a2cc
 ;
